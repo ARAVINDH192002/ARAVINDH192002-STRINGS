f906e59c64c9c22a71ab876377ee53ba1f9c16cc
ARAVINDH.K <87661167+ARAVINDH192002@users.noreply.github.com>
2021-09-30 16:05:21 +0530

    Create NelaBirthday.java

---
+++ NelaBirthday.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class NelaBirthday {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        int a=0,b=0;
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i)=='a')
+              a++;
+            else
+              b++;
+        }
+        if(a>b)
+          System.out.print(b);
+        else
+          System.out.print(a);
+    }
+}
