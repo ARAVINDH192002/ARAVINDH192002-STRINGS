72c12aca303d562e83420bf2548cfd4244994426
ARAVINDH.K <87661167+ARAVINDH192002@users.noreply.github.com>
2021-09-30 16:03:51 +0530

    Create little_author.java

---
+++ little_author.java
@@ -0,0 +1,22 @@
+ import java.util.*;
+ class l{
+     public static void main(String[] ar){
+         Scanner sc = new Scanner(System.in);
+       StringBuilder s=new StringBuilder(sc.nextLine());
+       int a=0,b=0;
+       for(int i=0;i<s.length();i++){
+           if(s.charAt(i)=='0'){
+               a++;
+            }
+            else{
+                b++;
+             }
+        }
+         if(a==1 || b==1){
+             System.out.print("Yes");
+           }
+         else{
+             System.out.print("No");
+         }
+     }
+}
